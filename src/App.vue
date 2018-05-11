3dd1cde35303bd29d20f51747744dd5c1e519879
Lars Maechel <lars.maechel@gmail.com>
2018-05-11 20:54:30 +0200

    Add AJAX request to youtube api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
     <div>
-        <SearchBar></SearchBar>
+        <SearchBar @termChange="onTermChange"></SearchBar>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     import SearchBar from './components/SearchBar';
+    import keys from '../config/keys';
     
     export default {
         name: 'App',
-        components: { SearchBar }
+        components: { SearchBar },
+        methods: {
+            onTermChange(searchTerm) {
+                axios.get(`https://www.googleapis.com/youtube/v3/search`, {
+                    params: {
+                        key: keys.API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm
+                    }
+                }).then(response => console.log(response));
+            }
+        }
     }
 </script>
 
